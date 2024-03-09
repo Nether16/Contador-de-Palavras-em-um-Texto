3ac28488477f04a734a2f1bf3e3fedda4a5446b9
Nether <enzonether@gmail.com>
2024-03-08 22:21:26 -0300

    Create Main.cs

---
+++ Main.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Escreva seu texto para saber quantas palavras a nele");
+        string texto = Console.ReadLine();
+
+        char[] numletras = texto.ToCharArray();
+        int numespaços = texto.Count(Char.IsWhiteSpace);
+
+        Console.WriteLine(numletras.Length - numespaços);
+    }
+}
